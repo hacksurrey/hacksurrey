f19af75329d64efa4ddfb6a4953c97712355ae2b
Ankur Mahto <ankurmahto1@gmail.com>
2019-08-07 18:12:02 +0100

    updated committee
    changed committee signatories and members to new team

--- components/Team.vue
+++ components/Team.vue
@@ -5,28 +5,20 @@
     </div>
     <div class="row center">
       <div class="item">
-        <img src="https://avatars2.githubusercontent.com/u/4354770?s=460&v=4" />
-        <span class="subtitle"><a href="https://twitter.com/sdaniel55" title="@sdaniel55">Dan</a><br/><span class="subsubtitle">President</span></span>
+        <img src="~resources/avatars/aleks.jpg" />
+        <span class="subtitle"><a href="https://www.linkedin.com/in/aleksandra-marevskaia-26a097176/" title="Aleksandra Marevskaia">Aleks</a><br/><span class="subsubtitle">President</span></span>
       </div>
       <div class="item">
-        <img src="https://avatars1.githubusercontent.com/u/7229472?s=460&v=4" />
-        <span class="subtitle"><a href="https://twitter.com/theomessin" title="@theomessin">Ted</a><br/><span class="subsubtitle">Vice President</span></span>
+        <img src="~resources/avatars/ankur.jpg" />
+        <span class="subtitle"><a href="https://www.linkedin.com/in/ankur-mahto-238916127" title="Ankur Mahto">Ankur</a><br/><span class="subsubtitle">Vice President</span></span>
       </div>
       <div class="item">
-        <img src="~resources/avatars/marwan.jpg" />
-        <span class="subtitle">Marwan<br/><span class="subsubtitle">Treasurer</span></span>
+        <img src="~resources/avatars/uliks.jpg" />
+        <span class="subtitle"><a href="https://www.linkedin.com/in/uliks-sekiraqa-438276149/" title="Uliks Seqiraqa">Uliks</a><br/><span class="subsubtitle">Treasurer</span></span>
       </div>
       <div class="item">
-        <img src="https://avatars1.githubusercontent.com/u/45294765?s=400&v=4" />
-        <span class="subtitle">Alex<br/><span class="subsubtitle">Committee</span></span>
-      </div>
-      <div class="item">
-        <img src="https://avatars2.githubusercontent.com/u/45466951?s=460&v=4" />
-        <span class="subtitle">Samantha<br/><span class="subsubtitle">Committee</span></span>
-      </div>
-      <div class="item">
-        <img src="https://avatars2.githubusercontent.com/u/33288730?s=400&v=4" />
-        <span class="subtitle">Kevin<br/><span class="subsubtitle">Committee</span></span>
+        <img src="~resources/avatars/rafail.jpg" />
+        <span class="subtitle"><a href="https://www.linkedin.com/in/rafail-giavrimis/" title="Rafail Giavrimis">Rafail</a><br/><span class="subsubtitle">Committee</span></span>
       </div>
     </div>
   </section>
