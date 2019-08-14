1724ae7a6458a9fac265413df11977ec705a3727
Ankur Mahto <ankurmahto1@gmail.com>
2019-08-14 19:15:12 +0100

    added member descriptions
    included a description for all committee members

--- components/Team.vue
+++ components/Team.vue
@@ -6,19 +6,27 @@
     <div class="row center">
       <div class="item">
         <img src="~resources/avatars/aleks.jpg" />
-        <span class="subtitle"><a href="https://www.linkedin.com/in/aleksandra-marevskaia-26a097176/" title="Aleksandra Marevskaia">Aleks</a><br/><span class="subsubtitle">President</span></span>
+        <span class="subtitle"><a href="https://www.linkedin.com/in/aleksandra-marevskaia-26a097176/" title="Aleksandra Marevskaia">Aleks</a><br/><span class="subsubtitle">President</span>
+        <p class="desc">I am a 2nd Year Computer Science student, and as the President I aiming to develop a conducive atmosphere within the society, as well as managing the day-to-day running of the society.</p>
+        </span>
       </div>
       <div class="item">
         <img src="~resources/avatars/ankur.jpg" />
-        <span class="subtitle"><a href="https://www.linkedin.com/in/ankur-mahto-238916127" title="Ankur Mahto">Ankur</a><br/><span class="subsubtitle">Vice President</span></span>
+        <span class="subtitle"><a href="https://www.linkedin.com/in/ankur-mahto-238916127" title="Ankur Mahto">Ankur</a><br/><span class="subsubtitle">Vice President</span>
+        <p class="desc">I am a 2nd Year Computer Science student, and as the Vice-President I am involved in running the society steadily, as well as assisting the president in organising engaging events for the members!</p>
+        </span>
       </div>
       <div class="item">
         <img src="~resources/avatars/uliks.jpg" />
-        <span class="subtitle"><a href="https://www.linkedin.com/in/uliks-sekiraqa-438276149/" title="Uliks Seqiraqa">Uliks</a><br/><span class="subsubtitle">Treasurer</span></span>
+        <span class="subtitle"><a href="https://www.linkedin.com/in/uliks-sekiraqa-438276149/" title="Uliks Seqiraqa">Uliks</a><br/><span class="subsubtitle">Treasurer</span>
+        <p class="desc">I'm  a 2nd Year Computer Science student, and as Treasurer I am in charge of managing the funds from our budget and sponsorships in order to host the fun events we have planned this year.</p>
+        </span>
       </div>
       <div class="item">
         <img src="~resources/avatars/rafail.jpg" />
-        <span class="subtitle"><a href="https://www.linkedin.com/in/rafail-giavrimis/" title="Rafail Giavrimis">Rafail</a><br/><span class="subsubtitle">Committee</span></span>
+        <span class="subtitle"><a href="https://www.linkedin.com/in/rafail-giavrimis/" title="Rafail Giavrimis">Rafail</a><br/><span class="subsubtitle">Committee</span>
+        <p class="desc">I'm a 2nd Year Computer Science student and as a Technical Expert I'm in charge of helping manage the technical infrastructure, social channels of HackSurrey and assisting in managing the website.<br/></p>
+        </span>
       </div>
     </div>
   </section>
@@ -54,12 +62,13 @@ section
   .item
     display: flex
     margin: 0 3px
+    width: 200px
     padding: .5em 0
     text-align: center
     flex-direction: column
     img
       margin: 0
-      width: 150px
+      width: 200px
     .subtitle
       padding: .35em
       color: #fcfcfc
@@ -72,6 +81,8 @@ section
         text-decoration: underline
       .subsubtitle
         font-size: .8em
+      .desc
+        font-size: .6em
 
 .center
   justify-content: center
